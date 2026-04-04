8094e3f804a4cc2678ff2fe2c496b863a781c7a2
ddiccoo <dodikchocolathoos@gmail.com>
2026-04-04 16:49:01 +0800

    terbaru

--- koneksi.php
+++ koneksi.php
@@ -9,4 +9,5 @@ $conn = mysqli_connect(hostname:'localhost',username:'root',password:'',database
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
 ?>
--- proses_simpan.php
+++ proses_simpan.php
@@ -16,4 +16,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 mysqli_close(mysql: $conn);
-?>;
+?>
